cd98eaf932c7d61c11cf1459260e7c4e999a42ed
Misha <drohomiretskiy@gmail.com>
2013-12-05 20:00:55 +0200

    оновленно

--- исходники/scriptg.php
+++ исходники/scriptg.php
@@ -6,7 +6,7 @@ echo '<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
 echo '<title>Test Form PHP.SU</title>';
 echo '</head>';
 echo '<body>';
-echo '<META HTTP-EQUIV=Refresh Content="0;URL=adminbd.html">';
+echo '<META HTTP-EQUIV=Refresh Content="0;URL=iftungInform.html">';
 echo '</body>';
 echo '</html>';
 ?>
